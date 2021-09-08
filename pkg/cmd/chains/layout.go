b247eca8edd2e84d43698bdaed1111847e1e3ccc
Parth Patel <parth@boxboat.com>
2021-09-08 17:27:25 -0400

    added inital layout creation from pipeline

---
+++ pkg/cmd/chains/layout.go
@@ -0,0 +1,40 @@
+package chains
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/tektoncd/cli/pkg/cli"
+)
+
+func layoutCommand(p cli.Params) *cobra.Command {
+	longHelp := ``
+
+	c := &cobra.Command{
+		Use:   "layout",
+		Short: "Print layout for a specific pipeline",
+		Long:  longHelp,
+		Annotations: map[string]string{
+			"commandType": "main",
+		},
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// Get the pipeline.
+			pipelineName := args[0]
+			path := args[1]
+
+			// Extract and decode the value of the payload annotation.
+			layoutPath, err := createIntotoLayout(p, pipelineName, path)
+			if err != nil {
+				return err
+			}
+
+			// Display the result.
+			fmt.Println(layoutPath)
+
+			return nil
+		},
+	}
+
+	return c
+}
